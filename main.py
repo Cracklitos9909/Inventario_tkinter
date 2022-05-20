92f408f5102ef4c85ff15e4064ec3a723c8a037e
Cracklitos9909 <77213646+Cracklitos9909@users.noreply.github.com>
2022-05-19 19:36:36 -0500

    :heart:

--- main.py
+++ main.py
@@ -4,6 +4,94 @@ import csv
 
 indice = 0
 
+def eliminar_producto():
+  ventana_eliminar_producto = tk.Tk()
+  ventana_eliminar_producto.title("Modificar producto")
+
+  etiqueta_identificador = tk.Label(ventana_eliminar_producto, text="Ingresa el codigo del producto que quieres eliminar:")
+  etiqueta_identificador.pack()
+  identificador = tk.Entry(ventana_eliminar_producto)
+  identificador.pack()
+
+  etiqueta_nombre = tk.Label(ventana_eliminar_producto,text = "Nombre del producto:")
+  etiqueta_nombre.pack()
+  entrada_nombre = tk.Entry(ventana_eliminar_producto)
+  entrada_nombre.pack()
+
+  etiqueta_cantidad = tk.Label(ventana_eliminar_producto,text = "Cantidad:")
+  etiqueta_cantidad.pack()
+  entrada_cantidad = tk.Entry(ventana_eliminar_producto)
+  entrada_cantidad.pack()
+
+  etiqueta_costo = tk.Label(ventana_eliminar_producto,text = "Costo del producto:")
+  etiqueta_costo.pack()
+  entrada_costo = tk.Entry(ventana_eliminar_producto)
+  entrada_costo.pack()
+
+  etiqueta_codigo = tk.Label(ventana_eliminar_producto,text = "Codigo del producto:")
+  etiqueta_codigo.pack()
+  entrada_codigo = tk.Entry(ventana_eliminar_producto)
+  entrada_codigo.pack()
+
+  etiqueta_descripcion = tk.Label(ventana_eliminar_producto,text = "Descripcion del producto:")
+  etiqueta_descripcion.pack()
+  entrada_descripcion = tk.Text(ventana_eliminar_producto, width=25, height=5)
+  entrada_descripcion.pack()
+
+  def eliminar_registro():
+    global indice
+    datos = []
+    with open("Inventario.csv", "r", newline="") as archivo:
+      lector = csv.reader(archivo, delimiter=",")
+      for lista in lector:
+        datos.append(lista)
+      datos.pop(indice)
+      indice = 0
+    
+    with open("Inventario.csv", "w", newline="") as archivo:
+      escritor = csv.writer(archivo, delimiter=",")
+      for registro in datos:
+        escritor.writerow(registro)
+
+  def buscar_producto():
+    try:
+      id = identificador.get()
+      with open("Inventario.csv", 'r', newline = "") as archivo:
+        lector = csv.reader(archivo, delimiter = ",")
+        bandera = False
+        registro_temporal = []
+        global indice 
+        for registro in lector:
+          if registro[3] == id:
+            registro_temporal = registro
+            bandera = True
+            break
+          indice += 1
+        if bandera:
+          entrada_nombre.insert(0, registro_temporal[0])
+          entrada_cantidad.insert(0, registro_temporal[1])
+          entrada_costo.insert(0, registro_temporal[2])
+          entrada_codigo.insert(0, registro_temporal[3])
+          entrada_descripcion.insert(1.0, registro_temporal[4])
+        else:
+          etiqueta_error = tk.Label(ventana_eliminar_producto, text="El producto no fue encontrado:)")
+          etiqueta_error.pack()
+    except:
+      etiqueta_error = tk.Label(ventana_eliminar_producto, text="Lo sentimos algo salio mal, por favor intente de nuevo :)")
+      etiqueta_error.pack()
+
+  boton_buscar = tk.Button(ventana_eliminar_producto ,text="Buscar producto", command=buscar_producto)
+  boton_buscar.pack()
+
+  boton_modificar_datos = tk.Button(ventana_eliminar_producto, text="Eliminar", command=eliminar_registro)
+  boton_modificar_datos.pack()
+
+  boton_salir = tk.Button(ventana_eliminar_producto ,text="Salir", command=ventana_eliminar_producto.destroy)
+  boton_salir.pack()
+
+  ventana_eliminar_producto.attributes("-fullscreen", True)
+  ventana_eliminar_producto.mainloop()
+
 def modificar_producto():
   ventana_modificar_producto = tk.Tk()
   ventana_modificar_producto.title("Modificar producto")
@@ -209,7 +297,7 @@ def main():
   elementos_menu.add_command(label = "Registrar producto", command = registrar_producto)
   elementos_menu.add_command(label = "Listar productos", command=listar_productos)
   elementos_menu.add_command(label = "Modificar producto", command=modificar_producto)
-  elementos_menu.add_command(label = "Eliminar producto")
+  elementos_menu.add_command(label = "Eliminar producto", command=eliminar_producto)
   elementos_menu.add_separator()
   elementos_menu.add_command(label = "Salir", command = ventana.destroy)
 
